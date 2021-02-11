0f489b70449418efa8c44ce84e2cced85f599c21
Director099 <ferz099@mail.ru>
2021-02-11 09:10:10 +0500

    first commit

---
+++ src/blocks/components/tags-list/tags-list.js
@@ -0,0 +1,5 @@
+const tagsList = () => {
+
+};
+
+export default tagsList;
---
+++ src/js/custom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// import './config/jqueryLoad';
+// import '@fancyapps/fancybox/dist/jquery.fancybox.min';
+// import '../blocks/components/dropdown/dropdown.js';
+
+var swiper = new Swiper('.js-main-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
+});
