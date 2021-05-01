b14d0957fbc1238e01e55cfea364d79012abe344
RedX <director099@gmail.com>
2021-05-01 19:13:04 +0500

    Автовысота textarea

--- build/js/custom.js
+++ build/js/custom.js
@@ -550,4 +550,17 @@
     });
   }
 
+  const fieldTextarea = $('.js-tx-auto-height');
+  fieldTextarea.on('keyup paste', function () {
+    var $el = $(this),
+        offset = $el.innerHeight() - $el.height();
+
+    if ($el.innerHeight() < this.scrollHeight) {
+      $el.height(this.scrollHeight - offset);
+    } else {
+      $el.height(1);
+      $el.height(this.scrollHeight - offset);
+    }
+  });
+
 })));
