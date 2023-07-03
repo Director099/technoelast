3e1a2011bb48dbf3fdb9bb58c3500834145147ee
redx099 <ferz099@mail.ru>
2023-07-03 08:30:17 +0500

    Доработан виджет

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// import './config/jqueryLoad';
-// import '@fancyapps/fancybox/dist/jquery.fancybox.min';
-// import '../blocks/components/dropdown/dropdown.js';
 import '../blocks/components/to-top/to-top.js';
 import '../blocks/components/field-file/field-file.js';
 import '../blocks/components/collapse/collapse.js';
